f844e5d02d0ee40a3cfd57ceb282ffefb916df00
kcagras <k.cagras@web.de>
2016-09-02 11:33:14 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,58 @@
 var express 		= require('express');
-var http 			= require ('http');
 var app     		= express();
 var morgan			= require ('morgan');
+var PythonShell 	= require ('python-shell');
+
+var options = {
+	scriptPath: './python'
+};
 
 var port = process.env.PORT || 8080;
+var enabled = false;
 
 app.use(morgan('dev'));
 var apiRoutes = express.Router();
+
+var pyshell = new PythonShell('import_detector_v_0_1.py', { mode: 'text'});
+
+
+pyshell.on('message', function(msg) {
+	console.log(""+msg);
+	pyshell.end(function (err) {
+		if (err) throw err;
+		console.log('finished');
+	});
+});
+
+
+/*
 
 //================================
 // API-ROUTE FOR ENDISABLING
 //================================
 
 apiRoutes.get('/endisable', function(req, res) {
-	res.status(200).send();
+	if (!enabled) {
+		PythonShell.run('import_detector_v_0_1.py', options, function (err, results) { 
+			if (err) 
+				throw err;
+		});
+		enabled = true;
+		console.log('Enabled Movement Detection.');
+		res.status(200).send();
+	}
+	else {
+		PythonShell.end('import_detector_v_0_1.py', options, function (err, results) { 
+			if (err) 
+				throw err;
+		});		
+		enabled = false;
+		console.log('Disabled Movement Detection.');
+		res.status(200).send();
+	}
 });
 
 app.use('/', apiRoutes);
 
 app.listen(port);
+*/
